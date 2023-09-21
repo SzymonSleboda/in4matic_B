52d18996f9fc499007ec8ba382528fabc4c263f7
SzymonSleboda <szymonskeboda@gmail.com>
2023-09-21 15:53:29 +0200

    feature/userController-update

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        error: "Email is already in use",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
